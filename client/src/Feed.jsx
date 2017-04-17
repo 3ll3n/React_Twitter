653403267f414b774fff37d9feaf9483371e5950
3ll3n <ellengrafton01@gmail.com>
2017-04-17 16:40:24 +0100

    Added sampleData and constructor to Feed.jsx

---
+++ client/src/Feed.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+var sampleData = [
+  {id: 1, username: 'Buffy', real_name: 'Buffy Summers', text: 'I am a vampire slayer'},
+  {id: 2, username: 'Angel', real_name: 'Liam', text: 'I am a good vampire'}
+];
+
+export default class Feed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: sampleData
+    };
+  }
+}
